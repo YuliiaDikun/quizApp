7625834fca49094b98d49810800672d157234c3b
YuliiaDikun <dikun.yulia@gmail.com>
2024-03-12 18:00:20 +0200

    add some questions

--- utils/subjectMap.ts
+++ utils/subjectMap.ts
@@ -109,15 +109,343 @@ const subjectMap = [
     },
     {
         title: 'articles',
-        questions: []
+        questions: [
+            {
+                title: 'Do you want _ drink?',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'I am going to take _ dog for a walk.',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'Have you seen _ car key?',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'It took me _ hour to get home.',
+                correct_answer: 1,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: '_ United States has many great vacation spots',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'A good education is important.',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'I found _ ant hill in the garden.',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "a",
+                    "an",
+                    'the',
+                ]
+            },
+            {
+                title: 'I have _ apple and _ orange for a snack.',
+                correct_answer: 3,
+                selected_answer: null,
+                answers: [
+                    "a, a",
+                    "an, a",
+                    "an, an",
+                    "a, an",
+                ]
+            },
+            {
+                title: 'She bought _ new dress for _ party.',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "a, an",
+                    "an, an",
+                    "a, a",
+                    "an, the",
+                ]
+            },
+            {
+                title: 'I need to buy _ laptop and _ smartphone.',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "a, a",
+                    "the, the",
+                    "a, an",
+                    "an, a",
+                ]
+            }
+        ]
     },
     {
         title: 'active-tenses',
-        questions: []
+        questions: [
+            [
+                {
+                    title: 'My sister always _____ (to arrive) late for work.',
+                    correct_answer: 1,
+                    selected_answer: null,
+                    answers: [
+                        "arrives",
+                        "arrive",
+                        "has arrived",
+                        "arriving",
+                    ]
+                },
+                {
+                    title: 'We usually _____ (to eat) dinner at 7 PM.',
+                    correct_answer: 0,
+                    selected_answer: null,
+                    answers: [
+                        "eat",
+                        "eating",
+                        "eats",
+                        "eaten",
+                    ]
+                },
+                {
+                    title: 'They _____ (to live) in London for five years now.',
+                    correct_answer: 2,
+                    selected_answer: null,
+                    answers: [
+                        "live",
+                        "living",
+                        "have been living",
+                        "lived",
+                    ]
+                },
+                {
+                    title: 'He usually _____ (to take) the bus to work.',
+                    correct_answer: 3,
+                    selected_answer: null,
+                    answers: [
+                        "takes",
+                        "take",
+                        "taken",
+                        "is taking",
+                    ]
+                },
+                {
+                    title: 'I _____ (to study) Spanish for two years.',
+                    correct_answer: 0,
+                    selected_answer: null,
+                    answers: [
+                        "have been studying",
+                        "study",
+                        "studied",
+                        "studying",
+                    ]
+                },
+                {
+                    title: 'She _____ (not to go) to the gym every day.',
+                    correct_answer: 2,
+                    selected_answer: null,
+                    answers: [
+                        "don't go",
+                        "doesn't go",
+                        "isn't going",
+                        "not go",
+                    ]
+                },
+                {
+                    title: 'We _____ (to watch) a movie tonight.',
+                    correct_answer: 1,
+                    selected_answer: null,
+                    answers: [
+                        "watch",
+                        "are watching",
+                        "watched",
+                        "watches",
+                    ]
+                },
+                {
+                    title: 'My parents _____ (to visit) us next month.',
+                    correct_answer: 0,
+                    selected_answer: null,
+                    answers: [
+                        "will visit",
+                        "visit",
+                        "visited",
+                        "are visiting",
+                    ]
+                },
+                {
+                    title: 'The sun _____ (to rise) in the east.',
+                    correct_answer: 3,
+                    selected_answer: null,
+                    answers: [
+                        "rise",
+                        "rises",
+                        "rising",
+                        "is rising",
+                    ]
+                },
+                {
+                    title: 'They _____ (to speak) French fluently.',
+                    correct_answer: 1,
+                    selected_answer: null,
+                    answers: [
+                        "speaking",
+                        "speak",
+                        "spoken",
+                        "speakable",
+                    ]
+                }
+            ]
+        ]
     },
     {
         title: 'passive-tenses',
-        questions: []
+        questions: [
+            {
+                title: 'The book _____ (to read) by many students.',
+                correct_answer: 1,
+                selected_answer: null,
+                answers: [
+                    "read",
+                    "is read",
+                    "reads",
+                    "has read",
+                ]
+            },
+            {
+                title: 'This song _____ (to sing) by a famous artist.',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "sings",
+                    "sing",
+                    "is singing",
+                    "sung",
+                ]
+            },
+            {
+                title: 'The new highway _____ (to open) next month.',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "open",
+                    "opens",
+                    "will be opened",
+                    "opened",
+                ]
+            },
+            {
+                title: 'A letter _____ (to write) by her every week.',
+                correct_answer: 3,
+                selected_answer: null,
+                answers: [
+                    "writes",
+                    "writing",
+                    "written",
+                    "is written",
+                ]
+            },
+            {
+                title: 'The cake _____ (to bake) for the celebration.',
+                correct_answer: 1,
+                selected_answer: null,
+                answers: [
+                    "bakes",
+                    "is baked",
+                    "baked",
+                    "bake",
+                ]
+            },
+            {
+                title: 'The Mona Lisa _____ (to paint) by Leonardo da Vinci.',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "paints",
+                    "painted",
+                    "is painting",
+                    "is painted",
+                ]
+            },
+            {
+                title: 'The report _____ (to prepare) by the team.',
+                correct_answer: 2,
+                selected_answer: null,
+                answers: [
+                    "prepare",
+                    "preparing",
+                    "will be prepared",
+                    "prepared",
+                ]
+            },
+            {
+                title: 'A new policy _____ (to announce) soon.',
+                correct_answer: 3,
+                selected_answer: null,
+                answers: [
+                    "announces",
+                    "is announcing",
+                    "announcing",
+                    "will be announced",
+                ]
+            },
+            {
+                title: 'The concert _____ (to attend) by thousands of fans.',
+                correct_answer: 1,
+                selected_answer: null,
+                answers: [
+                    "attends",
+                    "is attended",
+                    "attended",
+                    "attend",
+                ]
+            },
+            {
+                title: 'The building _____ (to construct) last year.',
+                correct_answer: 0,
+                selected_answer: null,
+                answers: [
+                    "constructs",
+                    "constructed",
+                    "constructing",
+                    "is constructed",
+                ]
+            }
+        ]
     },
     {
         title: 'gerund',
